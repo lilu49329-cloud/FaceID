3ebf047982f46a69fd7a1b8a06e610d61b2f91dd
lilu <lilu49329@gmail.com>
2026-04-16 20:32:41 +0700

    Fix mobile overflow, update button labels and refine header user info

--- push.bat
+++ push.bat
@@ -1,7 +1,4 @@
 @echo off
-git init
-git remote add origin https://github.com/lilu49329-cloud/FaceID
 git add .
-git commit -m "Update labels and fix header user info inconsistency"
-git branch -M main
-git push -u origin main
+git commit -m "Fix mobile overflow, update button labels and refine header user info"
+git push -u origin main -f
